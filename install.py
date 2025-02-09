7b852d8f8978ae6430a6e8899a58aeea37ba8ab8
Jin Yu <user1@jinui-MacBookAir.local>
2025-02-09 21:11:56 +0900

    Create install packages

---
+++ install.py
@@ -0,0 +1,15 @@
+pip install tensorflow
+pip install keras 
+pip install gensim
+pip install scikit-learn
+pip install jupyter
+
+pip instal nltk
+nltk.download()
+
+pip install konlpy
+
+
+pip install pandas 
+pip install numpy
+pip install matplotlib
